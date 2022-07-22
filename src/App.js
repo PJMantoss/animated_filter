09b7611c8dd4a3028cd98d2d9dedec5ab3e19042
PJMantoss <joelptoss@gmail.com>
2022-07-22 19:46:52 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 
 function App() {
+
+  const URL = 'https://api.themoviedb.org/3/movie/popular?api_key=aa3f395118eda0937800db0cba5a6b63&language=en-US&page=1';
+
+  try{}catch(err){console.log(err)};
+
   return (
     <div className="App">
       <h1>Hello</h1>
