a9195f8bb3d53b0d2b42f4bf223dcc90ffc6d044
PJMantoss <joelptoss@gmail.com>
2022-07-22 19:47:35 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
 
   const URL = 'https://api.themoviedb.org/3/movie/popular?api_key=aa3f395118eda0937800db0cba5a6b63&language=en-US&page=1';
 
-  try{}catch(err){console.log(err)};
+  try{
+    const fetchPopMovies = async () => {
+      const data = await fetch(URL);
+    };
+  }catch(err){console.log(err)};
 
   return (
     <div className="App">
