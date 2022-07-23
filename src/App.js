2c6b1c7d0dd35ddb5dba94f86de03bececc3a796
PJMantoss <joelptoss@gmail.com>
2022-07-23 18:30:16 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [popular, setPopular] = useState([]);
 
   const URL = 'https://api.themoviedb.org/3/movie/popular?api_key=aa3f395118eda0937800db0cba5a6b63&language=en-US&page=1';
 
-  try{
-    const fetchPopMovies = async () => {
-      const data = await fetch(URL);
-      const movies = await data.json();
-      console.log(movies);
-    };
-
+  useEffect(() => {
+      const fetchPopMovies = async () => {
+        const data = await fetch(URL);
+        const movies = await data.json();
+        console.log(movies);
+      };
     fetchPopMovies();
-  }catch(err){
-    console.log(err)
-  };
+  }, [])
 
   return (
     <div className="App">
