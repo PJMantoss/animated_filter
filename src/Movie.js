276cd0d50c57ebc71831a6c5bdef348c80bf7dd3
PJMantoss <joelptoss@gmail.com>
2022-07-24 17:18:30 +0100

    update Movie

--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 const Movie = ({movie}) => {
   return (
-    <div>
+    <motion.div layout>
         <h2>{movie.title}</h2>
         <img 
           src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} 
           alt='movie poster' 
         />
-    </div>
+    </motion.div>
   )
 }
 
