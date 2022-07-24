76515d0a9beb40871701240f06f60419ffff9598
PJMantoss <joelptoss@gmail.com>
2022-07-24 16:44:51 +0100

    update App

--- src/Filter.js
+++ src/Filter.js
@@ -5,12 +5,40 @@ const Filter = ({activeGenre, setActiveGenre, setFiltered, popular}) => {
     <div className='filterContainer'>
         <button 
             onClick={() => setActiveGenre(0)} 
-            classname=''
+            className=''
         >
             All
         </button>
-        <button classname=''>Comedy</button>
-        <button classname=''>Action</button>
+        <button 
+            onClick={() => setActiveGenre(28)} 
+            className=''
+        >
+            Action
+        </button>
+        <button 
+            onClick={() => setActiveGenre(35)} 
+            className=''
+        >
+            Comedy
+        </button>
+        <button 
+            onClick={() => setActiveGenre(16)} 
+            className=''
+        >
+            Animation
+        </button>
+        <button 
+            onClick={() => setActiveGenre(80)} 
+            className=''
+        >
+            Crime
+        </button>
+        <button 
+            onClick={() => setActiveGenre(12)} 
+            className=''
+        >
+            Adventure
+        </button>
     </div>
   )
 }
