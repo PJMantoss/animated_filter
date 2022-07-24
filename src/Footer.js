77127f3dc6991ba25df49745b662781cbab20127
PJMantoss <joelptoss@gmail.com>
2022-07-24 17:30:30 +0100

    add Footer

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+        <p><strong>Beer App</strong></p>
+        <p>Copyright 2020</p>
+
+        <div id="love">
+            <span>Made With</span>
+            <img src="https://img.icons8.com/dusk/64/000000/like.png" alt="love"/> 
+            <span>by <a href="https://github.com/PJMantoss" target="_blank">Mantoss</a></span>
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
