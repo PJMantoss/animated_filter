3580745122788644af838eb1fae1c234ed31ac12
PJMantoss <joelptoss@gmail.com>
2022-07-24 20:30:29 +0100

    update Movie

--- src/Movie.js
+++ src/Movie.js
@@ -6,7 +6,7 @@ const Movie = ({movie}) => {
     <motion.div 
       layout
       animate={{opacity: 1, scale: 1}}
-      initial={{opacity: 0}}
+      initial={{opacity: 0,scale:0}}
       exit={{opacity: 0}}
     >
         <h2>{movie.title}</h2>
